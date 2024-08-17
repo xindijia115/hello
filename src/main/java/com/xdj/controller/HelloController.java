4d808c84c8968df34b4e4661926782163adf44a9
DJ <xindijia@qt.com>
2024-08-17 13:03:15 +0800

    feat[CICD]:test03

--- src/main/java/com/xdj/controller/HelloController.java
+++ src/main/java/com/xdj/controller/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        String url = "http://www.xdj666.top";
+        String url = "https://www.xdj666.top";
         return url;
     }
 }
